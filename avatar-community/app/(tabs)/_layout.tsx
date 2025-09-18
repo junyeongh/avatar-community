194e69ff95ff543b1722c5d5ae658a3624d892ad
Junyeong Heo <hhjjyy1122@gmail.com>
2025-09-18 17:27:58 +0900

    [2-3] Using vectore icon
    [@expo/vector-icons@15.0.2](https://icons.expo.fyi/Index)

--- avatar-community/app/(tabs)/_layout.tsx
+++ avatar-community/app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { colors } from "@/constants";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 import React from "react";
 
@@ -14,18 +15,27 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? "home-sharp" : "home-outline"} size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="my"
         options={{
           title: "Profile",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? "person-circle" : "person-circle-outline"} size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
           title: "Settings",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? "settings" : "settings-outline"} size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
