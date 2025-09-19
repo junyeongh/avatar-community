6216f85ef275120054d938c370ea387842404daf
Junyeong Heo <hhjjyy1122@gmail.com>
2025-09-19 15:38:09 +0900

    [3-5] Implement Feed List component

--- avatar-community/app/(tabs)/index.tsx
+++ avatar-community/app/(tabs)/index.tsx
@@ -1,40 +1,21 @@
 // import Button from "@/components/CustomButton";
-import FeedItem from "@/components/FeedItem";
+import FeedList from "@/components/FeedList";
+import { colors } from "@/constants";
 // import { router } from "expo-router";
-import { Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>Home screen</Text>
-      {/* <Button
-        label="Button"
-        onPress={() => {
-          router.push("/auth");
-        }}
-      /> */}
-      <FeedItem
-        post={{
-          id: 1,
-          userId: 1,
-          title: "test title",
-          description:
-            "Cupidatat fugiat ea laboris incididunt eiusmod ullamco et Lorem occaecat est. Enim laborum cillum amet ea elit sunt ullamco ea non laboris. Fugiat voluptate anim laborum cupidatat quis. Sunt sunt esse anim cupidatat fugiat. Sit exercitation laboris irure aliquip consectetur eu sit. Ad consectetur elit irure commodo commodo dolor do incididunt commodo sint enim nisi labore reprehenderit. Occaecat commodo cupidatat aliqua esse adipisicing adipisicing aute adipisicing culpa velit non exercitation cupidatat laboris.",
-          createdAt: "test createdAt",
-          author: {
-            id: 1,
-            nickname: "this",
-            imageUri: "",
-          },
-          imageUris: [],
-          likes: [],
-          hasVote: false,
-          voteCount: 1,
-          commentCount: 1,
-          viewCount: 1,
-        }}
-      />
+    <SafeAreaView style={styles.container}>
+      <FeedList />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.WHITE,
+  },
+});
---
+++ avatar-community/components/FeedList.tsx
@@ -0,0 +1,64 @@
+import { colors } from "@/constants";
+import { FlatList, StyleSheet } from "react-native";
+import FeedItem from "./FeedItem";
+
+const test_data = [
+  {
+    id: 1,
+    userId: 1,
+    title: "test title",
+    description:
+      "Cupidatat fugiat ea laboris incididunt eiusmod ullamco et Lorem occaecat est. Enim laborum cillum amet ea elit sunt ullamco ea non laboris. Fugiat voluptate anim laborum cupidatat quis. Sunt sunt esse anim cupidatat fugiat. Sit exercitation laboris irure aliquip consectetur eu sit. Ad consectetur elit irure commodo commodo dolor do incididunt commodo sint enim nisi labore reprehenderit. Occaecat commodo cupidatat aliqua esse adipisicing adipisicing aute adipisicing culpa velit non exercitation cupidatat laboris.",
+    createdAt: "test createdAt",
+    author: {
+      id: 1,
+      nickname: "this",
+      imageUri: "",
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: "test title 2",
+    description:
+      "Cupidatat fugiat ea laboris incididunt eiusmod ullamco et Lorem occaecat est. Enim laborum cillum amet ea elit sunt ullamco ea non laboris. Fugiat voluptate anim laborum cupidatat quis. Sunt sunt esse anim cupidatat fugiat. Sit exercitation laboris irure aliquip consectetur eu sit. Ad consectetur elit irure commodo commodo dolor do incididunt commodo sint enim nisi labore reprehenderit. Occaecat commodo cupidatat aliqua esse adipisicing adipisicing aute adipisicing culpa velit non exercitation cupidatat laboris.",
+    createdAt: "test createdAt",
+    author: {
+      id: 1,
+      nickname: "this",
+      imageUri: "",
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+];
+
+interface FeedListProps {}
+
+export default function FeedList({}: FeedListProps) {
+  return (
+    <FlatList
+      data={test_data}
+      renderItem={({ item }) => <FeedItem post={item} />}
+      keyExtractor={(item) => String(item.id)}
+      contentContainerStyle={styles.contentContainer}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 12,
+    backgroundColor: colors.GRAY_200,
+  },
+});
